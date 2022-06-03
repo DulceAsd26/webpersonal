6245bf86b36e225dbae68dd5faf2fb0293afbf98
DulceAsd26 <juarez.dulce.1jm@gmail.com>
2022-06-03 00:10:00 -0500

    Template tag url

--- webpersonal/webpersonal/webpersonal/urls.py
+++ webpersonal/webpersonal/webpersonal/urls.py
@@ -19,8 +19,8 @@ from core import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('about/', views.about, name="about"),
-    path('portafolio/', views.portfolio, name="portafolio"),
+    path('about-me/', views.about, name="about"),
+    path('portfolio/', views.portfolio, name="portafolio"),
     path('contact/', views.contact, name="contact"),
     path('admin/', admin.site.urls),
 ]
