8ec4ed8bf8b55989c6582c52478836d844ba9c2a
DulceAsd26 <juarez.dulce.1jm@gmail.com>
2022-06-02 23:06:23 -0500

    Paginas portafolio y contacto

--- webpersonal/webpersonal/core/views.py
+++ webpersonal/webpersonal/core/views.py
@@ -20,3 +20,13 @@ def about(request):
         <h2>Acerca de</h2>
         <p>Me llamo Dulce y me encanta Django!</p>
     """)
+def contact(request):
+    return HttpResponse(html_base + """
+        <h2>Contacto</h2>
+        <p>Aquí os dejo mi email y mis redes sociales:</p>
+        <ul>
+            <li><a href="mailto:hola@hektorprofe.net">Email</a></li>
+            <li><a href="https://github.com/hcosta">Github</a></li>
+            <li><a href="https://youtube.com">Youtube</a></li>
+        </ul>
+    """)
--- webpersonal/webpersonal/webpersonal/urls.py
+++ webpersonal/webpersonal/webpersonal/urls.py
@@ -20,5 +20,6 @@ from core import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('about/', views.about, name="about"),
+    path('contact/', views.contact, name="contact"),
     path('admin/', admin.site.urls),
 ]
