cae1de361263f89b6b247d1d4603133e8d263ff4
DulceAsd26 <juarez.dulce.1jm@gmail.com>
2022-06-03 00:02:11 -0500

    Adaptando plantillas y herencias

--- webpersonal/webpersonal/webpersonal/urls.py
+++ webpersonal/webpersonal/webpersonal/urls.py
@@ -20,7 +20,7 @@ from core import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('about/', views.about, name="about"),
-    path('portfolio/', views.portfolio, name="portafolio"),
+    path('portafolio/', views.portfolio, name="portafolio"),
     path('contact/', views.contact, name="contact"),
     path('admin/', admin.site.urls),
 ]
